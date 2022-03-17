cdd23764990315f36dc1c0d6fa4ce3d87241e4a1
lampshed <aperta.no@gmail.com>
2022-03-17 15:54:20 +0000

    Updated index.tsx to move 'handleContentTypeForAppContent' function outside of the render && moved redux file from pages back into src file and updated imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,23 @@ import { useEffect } from "react";
 import AppContent from "src/components/AppContent";
 import NavigationLeft from "src/components/NavigationLeft";
 import styles from "src/styles/Index.module.scss";
-import store from "./redux/store";
+import store from "../redux/store";
 import { Provider } from "react-redux";
 export default class IndexPage extends React.Component<{}, {}> {
   state = {
     currContent: "",
   };
 
+  /* Grabbing input from child comp */
+  handleContentTypeForAppContent = (content: string) => {
+    this.setState({ currContent: content });
+  };
+
   public render() {
-    /* Grabbing input from child comp */
-    const handleContentTypeForAppContent = (content: string) => {
-      this.setState({ currContent: content });
-    };
     return (
       <div className=" h-[100vh] flex flex-row bg-[#F5F5FB]">
         <Provider store={store}>
-          <NavigationLeft passType={handleContentTypeForAppContent} />
+          <NavigationLeft passType={this.handleContentTypeForAppContent} />
           <AppContent content={this.state.currContent} />
         </Provider>
       </div>
